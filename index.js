f77c08e7a6a955ffb2baf2368c1663fb406e1be2
rbaek <rbaek@onshape.com>
2018-04-28 00:51:05 -0400

    Code cleanup, comments, and some additional tests

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 'use strict';
 
+//Heads and tails default to 0.
 var heads = 0;
 var tails = 0;
+//Empty array to store results
+var results  = new Array();
 
 console.log('Welcome to the coin flipping simulator!' + '\n' +
 			'Here are your results:'
 	);
+
 function coinFlip() {
 	var i;
+	//Flip coin 10 times, count the number of heads and tails
 	for (i = 0; i < 10; i++){
 		var flip = (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails';
-		console.log(flip)
 		if (flip == 'heads'){
-			heads++;;
+			//Push result to array.
+			results.push(flip);
+			//Add 1 to heads count.
+			heads++;
 		}
 		if (flip == 'tails'){
+			//Push result to array.
+			results.push(flip);
+			//Add 1 to tails count.
 			tails++;
 		}
 	}
@@ -22,11 +32,15 @@ function coinFlip() {
 }
 
 coinFlip()
-console.log('Number of heads:', heads + '\n' + 
-			'Number of tails:', tails);
+//For testing/debugging, print results and count.
+//console.log(results);
+//console.log('Number of heads:', heads + '\n' + 
+// 			'Number of tails:', tails);
 
+//Export for Mocha use.
 module.exports = { 
-	flip: coinFlip
-	//export the results of flip of heads or tails here
-	//also export the count
+	//Export flip function
+	flip: coinFlip,
+	//Export results array
+	results: results
 };
--- specs/coinSpec.js
+++ specs/coinSpec.js
@@ -1,16 +1,28 @@
 'use strict';
 
 const assert = require('assert');
-const flip = require('../index').flip;
+var chai = require('chai');
 //Reference the flip variable to get results
-const coin = require('../index').coin;
+const flip = require('../index').flip;
+//Reference results array from index
+var results = require('../index').results;
 
 describe('Heads or Tails', function(){
 	it('should be heads or tails', function(){
-		//Returns function.
-		//console.log('Coinflip:', flip);
-		//Should return the value of whether heads or tails, but always shows up undefined.
-		console.log('Coinflip:', coin);
+		//Test that the coin flip is heads or tails, should always be false.
 		assert.strictEqual(flip === 'heads' || flip === 'tails', false, 'result is either heads or tails');
+	});
+	it('should return array', function(){
+		//Test that the returned results is an array object.
+		chai.assert.isArray(results,'results is an array');
+	});
+	it('should contain heads and tails', function(){
+		//Test that the returned results array contains strings of results.
+		chai.assert.include(results, 'heads','array "results" includes string "heads"');
+		chai.assert.include(results, 'tails','array "results" includes string "tails"');
+	});
+	it('should have length of 10', function(){
+		//Test that the returned array has length of 10.
+		chai.assert.lengthOf(results, 10, 'array contains 10 results');
 	});
 });
